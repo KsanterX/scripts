dc75c654ae6c53e198668e145f6c65cff22e1c8b
Ksanter X <ksanterx@gmail.com>
2020-05-07 18:04:02 +0300

    Added powershell folder and vhdx creation for linux script

---
+++ ps1/vhdx_linux.ps1
@@ -0,0 +1,64 @@
+param (
+    [string]$path="",
+    [string]$diskname="linuxdisk.vhdx",
+    $size=127GB
+)
+
+If ($path -ne '') {
+$fullpath = (Join-Path $path $diskname)
+MainFunction
+} else {
+errors(0)
+}
+
+function MainFunction {
+if (checkPath) {
+    if (-Not (checkDisk)) {
+        createDisk
+        exit
+    } else {
+        yesnoSelector
+    }
+} else {
+mkdir
+MainFunction
+}
+}
+
+function mkdir {
+New-Item -ItemType Directory -Force -Path $path
+}
+
+function checkDisk {
+Test-Path $fullpath -PathType Leaf
+}
+
+function checkPath {
+  Test-Path $path -PathType Container
+}
+
+function yesnoSelector {
+errors(1)
+$Readhost = Read-Host "( y / n )"
+Switch ($Readhost)
+{
+Y {Remove-Item $fullpath; createDisk; exit}
+N {exit}
+}
+}
+
+function createDisk {
+New-VHD -Path $fullpath -SizeBytes $size -Dynamic -BlockSizeBytes 1MB
+}
+
+function errors ($code) {
+switch ($code)
+{
+0 {
+    'Please specify or edit the folder $path with optional $diskname (linuxdisk.vhdx) and $size (127GB)'
+}
+1 {
+    'Attention!!! Disk with this name already exists. Overwrite?'
+}
+}
+}
