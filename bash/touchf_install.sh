58c4c08f33a403499af42bb72ee3aec8dee69366
Ksanter X <ksanterx@gmail.com>
2020-05-18 10:19:19 +0300

    Minor fixes

--- bash/touchf_install.sh
+++ bash/touchf_install.sh
@@ -4,17 +4,26 @@ EX_FOLDER="/usr/local/bin"
 SCRIPT_NAME="touchf"
 
 function script_install() {
-  file_copy $SCRIPT_NAME $EX_FOLDER/$SCRIPT_NAME
-  chmod +x $EX_FOLDER/$SCRIPT_NAME
-  log "Installation of $SCRIPT_NAME finished."
-}
-
-function file_copy() {
-  cp $1 $2
+  if [ ! -d $EX_FOLDER ]
+  then
+    mkdir $EX_FOLDER
+    log "o1"
+  fi
+    cp $SCRIPT_NAME $EX_FOLDER/$SCRIPT_NAME
+    chmod +x $EX_FOLDER/$SCRIPT_NAME
+    log "o2"
 }
 
 function log()  {
-  echo $1
+  case $1 in
+  o1)
+    msg="Directory $EX_FOLDER created."
+    ;;
+  o2)
+    msg="Installation of $SCRIPT_NAME finished"
+    ;;
+  esac
+  echo $msg
 }
 
 script_install
